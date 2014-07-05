bb379e3fa9ca283bf63a8ff90cb0d13e9efa7373
mshevtsov <mikhail.shevtsov@gmail.com>
2014-07-05 18:59:18 +0400

    start

---
+++ README.md
@@ -0,0 +1,4 @@
+PHPixie-Party
+=============
+
+Please don't expect anything valuable from this module at this moment. I've just started developing it.
---
+++ classes/Party.php
@@ -0,0 +1,19 @@
+<?php
+namespace MShevtsov;
+
+Class Party {
+	public $pixie;
+	public $dirs = array();
+	public $rootDir;
+
+	public function __construct($pixie) {
+		$this->pixie = $pixie;
+		$this->rootDir = $this->pixie->root_dir;
+	}
+
+	// private function loadLibs() {
+	// 	$this->participant = new \MShevtsov\Party\Participant;
+	// 	$this->order = new \MShevtsov\Party\Order;
+	// 	$this->payment = new \MShevtsov\Party\Payment;
+	// }
+}
